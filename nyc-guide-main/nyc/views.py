60112ba0ff9ca21048800fed1c732826743c6336
Lee Harvey <337harveyWgmail.com>
2021-11-11 10:44:50 -0700

    modified for the inclusion of cards in the activity.html

--- nyc-guide-main/nyc/views.py
+++ nyc-guide-main/nyc/views.py
@@ -3,24 +3,28 @@ from .boroughs import boroughs
 import datetime
 
 def home(request):
-    return render(request, 'base.html', context={ 'boroughs': boroughs.keys() })
+    return render(request, 'base.html', context = { 'boroughs': boroughs.keys() })
     
 def city(request):
     if request.method == 'GET':
-        return render(request=request, template_name='city.html', context={ 'boroughs': boroughs.keys() })
+        return render(request=request, template_name = 'city.html', context = { 'boroughs': boroughs.keys() })
 
 def borough(request, borough):
     if request.method == 'GET':
-        return render(request=request, template_name='borough.html', context={ 'borough': borough, 'activities': boroughs[borough].keys() })
+        return render(request = request, template_name = 'borough.html', context = { 'borough': borough, 'activities': boroughs[borough].keys() })
 
 # Create your views here.
 def activity(request, borough, activity):
     if request.method == 'GET':
-        return render(request=request, template_name='activity.html', context={ 'borough': borough, 'activity': activity, 'places' : boroughs[borough][activity].keys() })
+        cards = {}
+        k = boroughs[borough][activity].keys()
+        for itm in k:
+            cards[itm] = boroughs[borough][activity][itm]
+        return render(request = request, template_name = 'activity.html', context = { 'borough': borough, 'activity': activity, 'places' : boroughs[borough][activity].keys(), 'cards' : cards })
 
 def venue(request, borough, activity, venue):
     if request.method == 'GET':
-        return render(request=request, template_name='venue.html', context={ 'borough': borough, 'activity': activity, 'place' : venue, 'venue' : boroughs[borough][activity][venue] })
+        return render(request = request, template_name = 'venue.html', context={ 'borough': borough, 'activity': activity, 'place' : venue, 'venue' : boroughs[borough][activity][venue] })
 
 def about(request):
     return render(request, 'about.html')
